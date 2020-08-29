7792aec2fd041ace9a60c38196cda9ef677ef406
Aurélien Kochalski <aurelien@strawberryunicorn.com>
2020-08-29 10:12:48 +0200

    Add custom font and its glyph optimizer (added to npm scripts)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ module.exports = {
         "./pages/**/*.js",
         "./components/**/*.js"
     ],
-    theme: {},
+    corePlugins: {
+        fontFamily: false // We will use custom fonts instead
+    },
+    theme: {
+        letterSpacing: { // We need more spacing between letters
+            tighter: "-.05em",
+            tight: "-.025em",
+            normal: "0",
+            wide: ".025em",
+            wider: ".1em",
+            widest: ".25em",
+        }
+    },
     variants: {},
     plugins: [],
 };
---
+++ utils/fontOptimizer.js
@@ -0,0 +1,27 @@
+const fontSrc = "font_custom/*.ttf";
+const fontDest = "public/fonts";
+
+// List of glyphs to keep inside the optimized font, the other glyphs are discarded on build
+const fontGlyphs = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789éèàêâùçÉÈÊÀ.,;:/\\+-_&@!?(){}[]\"'";
+
+var Fontmin = require("fontmin");
+
+var fontmin = new Fontmin()
+    .src(fontSrc)
+    .dest(fontDest)
+    .use(Fontmin.glyph({
+        text: fontGlyphs
+    }))
+    .use(Fontmin.ttf2woff({
+        deflate: true
+    }));
+    //.use(Fontmin.ttf2woff2());
+    //.use(Fontmin.ttf2eot())
+    //.use(Fontmin.ttf2svg());
+
+fontmin.run(function (err, files) {
+    if (err) {
+        throw err;
+    }
+    console.log(files);
+});
