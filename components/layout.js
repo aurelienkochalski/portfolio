4ab0894464003fd26e396ce27690f5e11618b50c
Aurélien Kochalski <aurelien@strawberryunicorn.com>
2020-09-08 17:23:22 +0200

    Add favicon

--- components/layout.js
+++ components/layout.js
@@ -13,7 +13,17 @@ export default function Layout(props) {
     return (
         <div className={classes}>
             <Head>
-                <link rel="icon" href="/favicon.ico" /> {/* TODO : change the favicon */}
+                {/* NOTE : Favicon generated using realfavicongenerator.net */}
+                <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
+                <link rel="manifest" href="/favicon/site.webmanifest"/>
+                <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#444444"/>
+                <link rel="shortcut icon" href="/favicon/favicon.ico"/>
+                <meta name="msapplication-TileColor" content="#777777"/>
+                <meta name="msapplication-config" content="/favicon/browserconfig.xml"/>
+                <meta name="theme-color" content="#ffffff"/>
+
                 <meta name="description" content=""/> {/* TODO : add meta tags */}
             </Head>
             <main>{props.children}</main>
