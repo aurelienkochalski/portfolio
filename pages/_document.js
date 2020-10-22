866accfe5c02b705385f0160b00fa2d60e87f43e
Aurélien Kochalski <aurelien.kochalski@gmail.com>
2020-10-22 15:33:51 +0200

    Font preloading for better first paint timing

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,12 @@ class MyDocument extends Document {
     render() {
         return (
             <Html lang="fr"> {/* TODO : Make the language dynamic */}
-            <Head />
+            <Head>
+                <link rel="preload" as="font" href="/fonts/custom-medium.woff" type="font/woff" crossorigin="anonymous" />
+                <link rel="preload" as="font" href="/fonts/custom-black.woff" type="font/woff" crossorigin="anonymous" />
+                <link rel="preload" as="font" href="/fonts/custom-light.woff" type="font/woff" crossorigin="anonymous" />
+                <link rel="preload" as="font" href="/fonts/font-icons.woff" type="font/woff" crossorigin="anonymous" />
+            </Head>
             <body>
                 <Main />
                 <NextScript />
